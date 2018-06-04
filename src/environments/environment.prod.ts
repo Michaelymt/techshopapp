fae630584c7897389dd4a1c5bd453266e6c439d3
Ivan <ivan.perez@pri.jovenclub.cu>
2018-06-04 12:24:10 -0400

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://glacial-thicket-23854.herokuapp.com/api',
+  apiUrlImage: 'https://glacial-thicket-23854.herokuapp.com/public/'
 };
