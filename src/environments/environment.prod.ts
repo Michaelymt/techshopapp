7823c9d56cd83955a59c50cdb7ef3264f54ffb26
Ivan <ivan.perez@pri.jovenclub.cu>
2018-06-14 12:53:02 -0400

    apiUrl fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://glacial-thicket-23854.herokuapp.com/api',
+  apiUrl: 'https://techshopapi.herokuapp.com/api',
   apiUrlImage: 'https://glacial-thicket-23854.herokuapp.com/public/'
 };
